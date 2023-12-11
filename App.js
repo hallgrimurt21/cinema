a67d5d6dcd847db29396abd7a7cb9b0e82bafa9e
hallgrimurt21 <hallgrimurth@gmail.com>
2023-12-11 11:00:30 +0000

    store placeholder

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 /* eslint-disable require-jsdoc */
 import React from "react"
 import {View} from "react-native"
-// import {Provider} from "react-redux"
-// import store from "./src/store"
+import {Provider} from "react-redux"
+import {store} from "./src/store"
 import {Main} from "./src/index"
 
 export default function App() {
     return (
-        <View>
+        <Provider store={store}>
             <Main />
-        </View>
+        </Provider>
     )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,12 @@
-// import {configureStore} from "@reduxjs/toolkit"
-// export const store = configureStore({
-// })
+import {configureStore} from "@reduxjs/toolkit"
+
+// This is a placeholder reducer
+const placeholderReducer = (state = {}, action) => {
+    return state
+}
+
+export const store = configureStore({
+    reducer: {
+        placeholder: placeholderReducer,
+    },
+})
