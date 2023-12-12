3e0e4e6431a7931ba758352528ae2903bd4c9e9f
hallgrimurt21 <hallgrimurth@gmail.com>
2023-12-12 13:21:01 +0000

    cinemaMovies

--- src/components/CinemaMovies/index.js
+++ src/components/CinemaMovies/index.js
@@ -1,18 +1,21 @@
-import React, {useEffect} from "react"
+import React from "react"
 import {View, Text, Pressable} from "react-native"
-import {useDispatch, useSelector} from "react-redux"
+import {useSelector} from "react-redux"
 import {useNavigation} from "@react-navigation/native"
 
 const CinemaMovies = ({id}) => {
     const movies = useSelector((state) => state.allMovies.movies)
-    const navigation = useNavigation()
+    const filteredMovies = movies.filter((movie) =>
+        movie.showtimes.some((showtime) => showtime.cinema.id === id),
+    )
+    const navigate = useNavigation().navigate
 
     const handlePress = (movieID) => {
-        navigation.navigate("Movie", {movieID, cinemaID: id})
+        navigate("Movie", {movieID, cinemaID: id})
     }
     return (
         <View>
-            {movies.map((movie) => (
+            {filteredMovies.map((movie) => (
                 <View key={movie.id}>
                     <Pressable onPress={() => handlePress(movie.id)}>
                         <Text>{movie.title}</Text>
