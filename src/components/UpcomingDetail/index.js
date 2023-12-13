5344358b805ba86de7ffd24f38b23c6acd10639b
Ingi Kr <ingik21@ru.is>
2023-12-13 09:58:22 +0000

    Youtube

--- src/components/UpcomingDetail/index.js
+++ src/components/UpcomingDetail/index.js
@@ -1,8 +1,11 @@
-import React from "react"
+/* eslint-disable max-len */
+import React, {useEffect, useState} from "react"
 import {View, Text, Pressable} from "react-native"
 import {useSelector} from "react-redux"
 import {useNavigation} from "@react-navigation/native"
 import {SafeAreaView} from "react-native-safe-area-context"
+import YoutubePlayer from "react-native-youtube-iframe"
+
 
 const UpcomingDetail = ({route}) => {
     const {id} = route.params
@@ -10,6 +13,15 @@ const UpcomingDetail = ({route}) => {
     const navigate = useNavigation().navigate
     const upcomingMovies = useSelector((state) => state.upcomingMovies.movies)
     const upcoming = upcomingMovies.find((movie) => movie._id === id)
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+
+        return () => {
+            setIsMounted(false)
+        }
+    }, [])
 
 
     console.log("upcoming 1: ", upcoming)
@@ -21,6 +33,7 @@ const UpcomingDetail = ({route}) => {
         return <Text>Loading...</Text>
     }
 
+
     return (
         <SafeAreaView>
             <View>
@@ -29,10 +42,15 @@ const UpcomingDetail = ({route}) => {
                 </Pressable>
                 <Text>{upcoming.title}</Text>
                 <Text>{upcoming["release-dateIS"]}</Text>
+                {isMounted && upcoming.trailers && upcoming.trailers[0] && upcoming.trailers[0].results && upcoming.trailers[0].results[0] && upcoming.trailers[0].results[0].key && (
+                    <YoutubePlayer
+                        height={300}
+                        play={false}
+                        videoId={upcoming.trailers[0].results[0].key}
+                    />
+                )}
             </View>
         </SafeAreaView>
-
-
     )
 }
 
