bf6cceed38d8be1e68c30b054a616455e4d8ebd8
Ingi Kr <ingik21@ru.is>
2023-12-13 16:12:10 +0000

    trailerinprogress

--- src/views/Movie/index.js
+++ src/views/Movie/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from "react"
 import {
     View,
@@ -31,17 +32,17 @@ function Movie({route}) {
             dispatch(hideAll())
         } else {
             switch (section) {
-                case "plot":
-                    dispatch(showPlot())
-                    break
-                case "genres":
-                    dispatch(showGenres())
-                    break
-                case "showtimes":
-                    dispatch(showShowtimes())
-                    break
-                default:
-                    break
+            case "plot":
+                dispatch(showPlot())
+                break
+            case "genres":
+                dispatch(showGenres())
+                break
+            case "showtimes":
+                dispatch(showShowtimes())
+                break
+            default:
+                break
             }
         }
     }
@@ -135,6 +136,17 @@ function Movie({route}) {
                         </Pressable>
                     ))}
                 </View>
+            )}
+            {movie.trailers && movie.trailers[0] && movie.trailers[0].results && movie.trailers[0].results[0] && movie.trailers[0].results[0].key && (
+                <Pressable
+                    style={({pressed}) => [
+                        {opacity: pressed ? 0.5 : 1},
+                        styles.trailerButton,
+                    ]}
+                    onPress={() => navigation.navigate("Trailer", {trailerID: movie.trailers[0].results[0].key})}
+                >
+                    <Text style={styles.time}>Watch Trailer</Text>
+                </Pressable>
             )}
         </ScrollView>
     )
---
+++ src/views/Trailer/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from "react-native"
+
+
+styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: "black",
+        height: 300,
+    },
+
+})
+export default styles
