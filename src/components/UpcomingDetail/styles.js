e0d9aa6cbee8c587383e3fdfeb84731f61ee2fec
HalliS2o <hallist@hey.com>
2023-12-16 13:31:29 +0000

    updated stylesheet for upcoming Details

--- src/components/UpcomingDetail/styles.js
+++ src/components/UpcomingDetail/styles.js
@@ -1,7 +1,12 @@
 import {StyleSheet} from "react-native"
-
-import {strongGrey, strongWhite} from "../../styles/colors"
-import {deviceWidth} from "../../styles/sizes"
+import {
+    mediumWhite,
+    mediumGrey,
+    strongGrey,
+    strongWhite,
+    crimson,
+} from "../../styles/colors"
+import {deviceHeight, deviceWidth} from "../../styles/sizes"
 
 const styles = StyleSheet.create({
     container: {
@@ -23,10 +28,12 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
     },
+
     rightInfo: {
         justifyContent: "flex-end",
         alignItems: "flex-end",
     },
+
     back: {
         color: strongWhite,
         fontSize: 20,
@@ -40,6 +47,7 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         color: strongWhite,
         textAlign: "right",
+
         width: "100%",
     },
     year: {
@@ -47,6 +55,92 @@ const styles = StyleSheet.create({
         color: strongWhite,
         textAlign: "right",
         width: "100%",
+    },
+    plot: {
+        color: mediumWhite,
+        textAlign: "center",
+        padding: 10,
+    },
+    genre: {
+        color: mediumWhite,
+        fontSize: 20,
+        textAlign: "center",
+        padding: 10,
+    },
+    allShows: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        shadowColor: "#000000",
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        marginTop: 10,
+    },
+    showtime: {
+        backgroundColor: mediumGrey,
+        padding: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "33.33%",
+    },
+    genres: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+    },
+    buttonRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "#000000",
+        shadowColor: "#000000",
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+    },
+    botButton: {
+        backgroundColor: mediumGrey,
+        flex: 1,
+        padding: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        height: 50,
+    },
+    botButText: {
+        color: mediumWhite,
+    },
+    time: {
+        color: mediumWhite,
+        fontSize: 15,
+    },
+    buy: {
+        color: mediumWhite,
+        fontSize: 10,
+    },
+    ytContainer: {
+        flexDirection: "row",
+    },
+
+    modalOverlay: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modal: {
+        width: "100%",
+        height: 200,
+        backgroundColor: crimson,
+    },
+    trailerButton: {
+        backgroundColor: mediumGrey,
+        marginTop: 10,
+        padding: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        height: 50,
+    },
+    extraView: {
+        height: deviceHeight,
     },
 })
 
